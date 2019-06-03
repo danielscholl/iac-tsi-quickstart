5a91d9cacdeced3c034b3d823bd3e904632b9460
Daniel Scholl <daniel.scholl@microsoft.com>
2019-06-03 12:59:17 -0500

    Initial Code

---
+++ README.md
@@ -0,0 +1,56 @@
+# Introduction
+Infrastructure as Code using ARM - Time Series Solution using ADSL Gen 2.
+
+
+
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fdanielscholl%2Fiot-tsi-quickstart%2Fmaster%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+
+
+## Getting Started
+
+1. Run Install Script for ARM Process
+
+```bash
+# Initialize the Modules
+contact="<your_initials>"
+install.sh $contact
+```
+
+
+# Build and Test 
+
+>NOTE:  THIS CAN ONLY BE DONE FROM A LINUX SHELL!!
+
+1. Manually run the test suite
+
+```bash
+npm install
+npm test
+```
+
+2. Manually provision the infrastructure.
+
+```bash
+# Provision the infrastructure
+npm run provision
+```
+
+# Azure Pipelines
+
+1. Build Definition
+
+    This project uses YAML builds.  A build would typically be created automatically but if not then a new Definition can be created and then import the `.vsts-ci.yml` YAML build file.
+
+    - Ensure that the proper Project Level Service Connection exists to be able to utilize the desired Subscription
+
+1. Release Definition
+
+    This project uses imported releases. A release is created with by importing the `.vsts-cd.json` file.  Some manual steps are required  to complete a valid pipeline.
+
+    - Ensure that the proper Project Level Service Connection exists to be able to utilize the desired Subscription
+    - Link the Artifact to the Build Artifact
+    - Select the Build Agent `Windows Containers`
+  
---
+++ tests/template.spec.js
@@ -0,0 +1,63 @@
+require('tap').mochaGlobals();
+const should = require('should');
+
+const template = require('../azuredeploy.json');
+const parameters = require('../azuredeploy.parameters.json');
+
+describe('iac-tsi-quickstart', () => {
+  context('template file', () => {
+    it('should exist', () => should.exist(template));
+
+    context('has expected properties', () => {
+      it('should have property $schema', () => should.exist(template.$schema));
+      it('should have property contentVersion', () => should.exist(template.contentVersion));
+      it('should have property parameters', () => should.exist(template.parameters));
+      it('should have property variables', () => should.exist(template.variables));
+      it('should have property resources', () => should.exist(template.resources));
+      it('should have property outputs', () => should.exist(template.outputs));
+    });
+
+    context('defines the expected parameters', () => {
+      const actual = Object.keys(template.parameters);
+
+      it('should have 3 parameters', () => actual.length.should.be.exactly(3));
+      it('should have a initials', () => actual.should.containEql('initials'));
+      it('should have a random', () => actual.should.containEql('random'));
+      it('should have a storageAccountType', () => actual.should.containEql('storageAccountType'));
+    });
+
+    context('creates the expected resources', () => {
+      const actual = template.resources.map(resource => resource.type);
+      const securityGroups = actual.filter(resource => resource === 'Microsoft.Network/networkSecurityGroups');
+
+      it('should have 1 resources', () => actual.length.should.be.exactly(1));
+      it('should create Microsoft.Storage/storageAccounts', () => actual.should.containEql('Microsoft.Storage/storageAccounts'));
+    });
+
+    context('storage has expected properties', () => {
+      const storage = template.resources.find(resource => resource.type === 'Microsoft.Storage/storageAccounts');
+
+      it('should define accessTier', () => should.exist(storage.properties.accessTier));
+      it('should define supportsHttpsTrafficOnly', () => should.exist(storage.properties.supportsHttpsTrafficOnly));
+      it('should define isHnsEnabled', () => should.exist(storage.properties.isHnsEnabled));
+      it('should have ADSL Gen2 Enabled', () => storage.properties.isHnsEnabled.should.be.true);
+    });
+
+    context('has expected output', () => {
+      it('should define storageAccount', () => should.exist(template.outputs.storageAccount));
+      it('should define storageAccount id', () => should.exist(template.outputs.storageAccount.value.id));
+      it('should define storageAccount name', () => should.exist(template.outputs.storageAccount.value.name));
+      it('should define storageAccount key', () => should.exist(template.outputs.storageAccount.value.key));
+    });
+  });
+
+  context('parameters file', (done) => {
+    it('should exist', () => should.exist(parameters));
+
+    context('has expected properties', () => {
+      it('should define $schema', () => should.exist(parameters.$schema));
+      it('should define contentVersion', () => should.exist(parameters.contentVersion));
+      it('should define parameters', () => should.exist(parameters.parameters));
+    });
+  });
+});
